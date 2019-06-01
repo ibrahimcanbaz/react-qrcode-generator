c9d0adb9791ae75688fef32ced87e4ccf36e9a61
ibrahimcanbaz <canbaz@canbaz.net>
2019-06-01 14:13:26 +0300

    Updates

---
+++ precache-manifest.358b9063207e5ccf8c454d030932d96e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c6578fd41c52e79ac63e12e7e80e75",
+    "url": "/react-qrcode-generator/index.html"
+  },
+  {
+    "revision": "8262c85dbdecf9076321",
+    "url": "/react-qrcode-generator/static/css/main.0724011d.chunk.css"
+  },
+  {
+    "revision": "a193a812a994a3200d03",
+    "url": "/react-qrcode-generator/static/js/2.7ddfd760.chunk.js"
+  },
+  {
+    "revision": "8262c85dbdecf9076321",
+    "url": "/react-qrcode-generator/static/js/main.d85f2639.chunk.js"
+  },
+  {
+    "revision": "a49e90b6f98e04f84f09",
+    "url": "/react-qrcode-generator/static/js/runtime~main.9e672d6b.js"
+  }
+]);
